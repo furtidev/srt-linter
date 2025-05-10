6601726f6f17c3fd3ca10763d25f4ab43e049aed
furtidev <furtidev@tuta.io>
2025-05-10 07:48:26 +0600

    tests(lexer): fix test

--- tests/lexer_test.rs
+++ tests/lexer_test.rs
@@ -32,26 +32,26 @@ fn test_token_generation_and_lexer_linting() {
             Token::Count(1, 1),
             Token::StartTime(136612, 2),
             Token::EndTime(139376, 2),
-            Token::Subtitle(vec![
+            Token::Subtitle((vec![
                 "Senator, we're making".into(),
                 "our final approach into Coruscant.".into()
-            ]),
+            ], 3)),
             Token::Count(2, 6),
             Token::StartTime(139482, 7),
             Token::EndTime(141609, 7),
-            Token::Subtitle(vec!["Very good, Lieutenant.".into()]),
+            Token::Subtitle((vec!["Very good, Lieutenant.".into()], 8)),
             Token::Count(3, 10),
             Token::StartTime(193336, 11),
             Token::EndTime(195167, 11),
-            Token::Subtitle(vec!["We made it.".into()]),
+            Token::Subtitle((vec!["We made it.".into()], 12)),
             Token::Count(4, 14),
             Token::StartTime(198608, 15),
             Token::EndTime(200371, 15),
-            Token::Subtitle(vec!["I guess I was wrong.".into()]),
+            Token::Subtitle((vec!["I guess I was wrong.".into()], 16)),
             Token::Count(5, 18),
             Token::StartTime(200476, 19),
             Token::EndTime(202671, 19),
-            Token::Subtitle(vec!["There was no danger at all.".into()]),
+            Token::Subtitle((vec!["There was no danger at all.".into()], 20)),
             Token::Eof,
         ]
     );
